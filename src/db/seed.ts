8929e3ce553b47c2f3f8bcfc6e679cb817d52e3c
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-13 15:06:37 -0300

    feat: added linters configuration

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,3 +1,5 @@
+/* eslint-disable drizzle/enforce-delete-with-where */
+
 import { faker } from '@faker-js/faker'
 import chalk from 'chalk'
 import { db } from './connection'
@@ -13,44 +15,47 @@ console.log(chalk.yellow('✅ Database reset!'))
 
 /**
  * Create customers
-*/
+ */
 await db.insert(users).values([
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: 'customer'
+    role: 'customer',
   },
   {
     name: faker.person.fullName(),
     email: faker.internet.email(),
-    role: 'customer'
-  }
+    role: 'customer',
+  },
 ])
 
 console.log(chalk.yellow('✅ Created customers!'))
 
 /**
  * Create manager
-*/
-const [manager] = await db.insert(users).values([
-  {
-    name: faker.person.fullName(),
-    email: 'admin@admin.com',
-    role: 'manager'
-  }
-]).returning({ id: users.id })
+ */
+const [manager] = await db
+  .insert(users)
+  .values([
+    {
+      name: faker.person.fullName(),
+      email: 'admin@admin.com',
+      role: 'manager',
+    },
+  ])
+  .returning({ id: users.id })
 
 console.log(chalk.yellow('✅ Created manager!'))
 
 /**
  * Create restaurant
-*/
+ */
 await db.insert(restaurants).values([
   {
     name: faker.company.name(),
     description: faker.lorem.paragraph(1),
-    managerId: manager.id
-  }
+    managerId: manager.id,
+  },
 ])
 
 console.log(chalk.yellow('✅ Created manager!'))
