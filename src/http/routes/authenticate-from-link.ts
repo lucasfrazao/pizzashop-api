9d5f0fa1af3ecb4d828c50d0bfcd44582009c308
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-14 17:53:50 -0300

    feat: added signout functionality

--- src/http/routes/authenticate-from-link.ts
+++ src/http/routes/authenticate-from-link.ts
@@ -7,7 +7,7 @@ import { auth } from '../auth'
 
 export const authenticateFromLink = new Elysia().use(auth).get(
   '/auth-links/authenticate',
-  async ({ query, jwt, cookie: { auth }, set }) => {
+  async ({ query, set, signUser }) => {
     const { code, redirect } = query
 
     const authLinkFromCode = await db.query.authLinks.findFirst({
@@ -29,21 +29,16 @@ export const authenticateFromLink = new Elysia().use(auth).get(
       throw new Error('Auth link expired, please generate a new one.')
     }
 
-    const managedRestaurante = await db.query.restaurants.findFirst({
+    const managedRestaurant = await db.query.restaurants.findFirst({
       where(fields, { eq }) {
         return eq(fields.managerId, authLinkFromCode.userId)
       },
     })
 
-    const token = await jwt.sign({
+    await signUser({
       sub: authLinkFromCode.userId,
-      restaurantId: managedRestaurante?.id,
+      restaurantId: managedRestaurant?.id,
     })
-
-    auth.value = token
-    auth.httpOnly = true
-    auth.maxAge = 60 * 60 * 24 * 7 // 7 days
-    auth.path = '/'
 
     await db.delete(authLinks).where(eq(authLinks.code, code))
     set.redirect = redirect
