18ae1002560cdaf63f7be5720463191a8aa11ac3
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-15 09:36:54 -0300

    feat: create get managed restaurant route

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getProfile } from './routes/get-profile'
 import { signOut } from './routes/sign-out'
 
@@ -13,6 +14,7 @@ const app = new Elysia()
   .use(authenticateFromLink)
   .use(signOut)
   .use(getProfile)
+  .use(getManagedRestaurant)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running')
