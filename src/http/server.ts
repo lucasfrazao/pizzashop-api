885e9d33f7517dd18602e986dc5db0fc383d45f0
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-15 09:19:52 -0300

    feat: create get profile route

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,6 +4,7 @@ import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { getProfile } from './routes/get-profile'
 import { signOut } from './routes/sign-out'
 
 const app = new Elysia()
@@ -11,6 +12,7 @@ const app = new Elysia()
   .use(sendAuthLink)
   .use(authenticateFromLink)
   .use(signOut)
+  .use(getProfile)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running')
