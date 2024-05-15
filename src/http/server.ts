a4fb3cb8c6e784d36ff11ab949a55ff410434358
Lucas Frazão <lucasfraza0@outlook.com>
2024-05-15 14:18:10 -0300

    feat: update status order

--- src/http/server.ts
+++ src/http/server.ts
@@ -3,7 +3,11 @@ import { Elysia } from 'elysia'
 import { registerRestaurant } from './routes/register-restaurant'
 import { sendAuthLink } from './routes/send-auth-link'
 
+import { approveOrder } from './routes/approve-order'
 import { authenticateFromLink } from './routes/authenticate-from-link'
+import { cancelOrder } from './routes/cancel-order'
+import { deliverOrder } from './routes/deliver-order'
+import { dispatchOrder } from './routes/dispatch-order'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getOrderDetails } from './routes/get-order-details'
 import { getProfile } from './routes/get-profile'
@@ -17,6 +21,10 @@ const app = new Elysia()
   .use(getProfile)
   .use(getManagedRestaurant)
   .use(getOrderDetails)
+  .use(approveOrder)
+  .use(cancelOrder)
+  .use(deliverOrder)
+  .use(dispatchOrder)
   .onError(({ code, error, set }) => {
     switch (code) {
       case 'VALIDATION': {
